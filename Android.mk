7ac84b250f5f36178a993ffd700671fc792d7428
John Patrick <manalojohnpatricksangalang@gmail.com>
2026-08-09 03:27:13 +0000

    Add AML_ImGui as submodule

--- Android.mk
+++ Android.mk
@@ -7,8 +7,14 @@ ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
 else
     LOCAL_MODULE := CarAudio64
 endif
+
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
-LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/aml-psdk-gtasa $(LOCAL_PATH)/aml-psdk-gtasa/aml-psdk/game_sa
+
+# Idagdag ang AML_ImGui folder dito
+LOCAL_C_INCLUDES += \
+    $(LOCAL_PATH)/AML_ImGui \
+    $(LOCAL_PATH)/aml-psdk-gtasa \
+    $(LOCAL_PATH)/aml-psdk-gtasa/aml-psdk/game_sa
+
 LOCAL_LDLIBS += -llog
 include $(BUILD_SHARED_LIBRARY)
